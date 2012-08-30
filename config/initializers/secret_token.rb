207e294a1393184a09de3c8cb61e485af1652346
pramendra <pramendra@gmail.com>
2012-08-30 14:18:34 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bizsity::Application.config.secret_token = 'b6ac043aa2a74dd5577037094ebb77e813a5b781258ef88fd7495d480e11d840db07bc93a96e090bd98e89275531eb7cbd946163c960c74013bec2f6f932e4e9'
